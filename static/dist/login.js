1557e528349f22138edcf373a0218ae2c816acf8
cond0r@desktop <zlc@zhao.cl>
2022-04-08 23:11:32 +0800

    增加不支持手机访问

--- static/dist/login.js
+++ static/dist/login.js
@@ -1,7 +1,23 @@
 function onclick_input(){
+    if(isMobile()){
+        return;
+    }
     window.location.href="/down?t="+Date.now()
     return false;
 }
 function login_btn(){
+    if(isMobile()){
+        return;
+    }
     alert("请输入帐号密码")
 }
+function isMobile(){
+    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+        // 当前设备是移动设备
+        alert("暂不支持手机访问");
+        return true;
+    }
+    return false;
+}
+
+
